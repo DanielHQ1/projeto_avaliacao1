f8957a8f1e056b5114d93d10fe927d76c3ee6287
DanielHQ1 <daniel.hq.carvalho@gmail.com>
2025-09-15 12:55:36 -0300

    Corrige declaração da matriz polinomio para usar 2 linhas

--- projeto_p1.c
+++ projeto_p1.c
@@ -2,64 +2,67 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define colunas 50
+
+
+void escreve_grau_polinomio(int grau_poli, int polinomio[][colunas])
+{
+    for (int k = grau_poli, i = 0; k >= 0; k--, i++)
+    {
+        polinomio[1][i] = k;
+    }
+}
+
 int main()
 {
-    // parei parei
-    bool continuar_atribuicao = true;
-    int polinomio[1][50];
-    int grau_poli;
-    int numero;
 
-    for (int i = 0; i <= 1; i++)
-    {
-        for (int k = 0; k <= 50; k++)
-        {
-            polinomio[i][k] = k;
-        }
-    }
+    // bool continuar_atribuicao = true;
+    int polinomio[2][colunas];
+    int grau_poli;
+    int entrada_numero;
+
+
+
+
+
+
+
+
+
+
 
     printf("Digite qual o grau do polinomio a ser recebido pelo programa: \n");
     if (scanf("%d", &grau_poli) != 1)
     {
         printf("Entrada invalida! Encerrando programa.\n");
     }
+    escreved_grau_polinomio(grau_poli, polinomio);
 
-    while (getchar() != '\n')
-        ;
 
-    for (int k = 0; k <= grau_poli; k++)
+        
+
+    for (int k = 0, j = grau_poli; k <= grau_poli; k++, j--)
     {
-        while (1)
-        {
-            printf("Digite o coeficiente %d do polinomio:\n", k);
 
-            if (scanf("%d", &numero) == 1)
+        
+            printf("Digite o coeficiente de x elevado a %d do polinomio:\n", j);
+            // serve para apenas ses entrada do numero for inteiro adicionar na lista
+            if (scanf("%d", &entrada_numero) == 1)
             {
-                polinomio[0][k] = numero;
-                while (getchar() != '\n')
-                    ;
-                break;
+                polinomio[0][k] = entrada_numero;
+
             }
-            else
-            {
-                printf("Entrada invalida! Digite um numero inteiro.\n");
-                while (getchar() != '\n')
-                    ;
-            }
-        }
+
+        
     }
 
-    for (int k = grau_poli, i = 0; k >= 0; k--, i++)
-    {
-        polinomio[0][i] = k;
-    }
+    
+
 
     printf("\nPolinomio montado:\n");
     for (int k = 0; k <= grau_poli; k++)
     {
         printf("%dx^%d ", polinomio[0][k], polinomio[1][k]);
-        if (k < grau_poli)
-            printf("+ ");
     }
     printf("\n");
 
